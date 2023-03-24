50b7726100394a6bf213b228a32146573e5e61b4
Ana R <anar@Anas-MacBook-Air.local>
2023-03-24 12:45:49 -0400

    horiscope

---
+++ js/main.js
@@ -0,0 +1,37 @@
+function getZodiac() {
+    const birthdate = new Date(document.getElementById("birthdate").value);
+    const month = birthdate.getMonth() + 1;
+    const day = birthdate.getDate();
+  
+    let zodiacSign = "";
+  
+    if ((month == 3 && day >= 21) || (month == 4 && day <= 19)) {
+      zodiacSign = "Aries";
+    } else if ((month == 4 && day >= 20) || (month == 5 && day <= 20)) {
+      zodiacSign = "Taurus";
+    } else if ((month == 5 && day >= 21) || (month == 6 && day <= 20)) {
+      zodiacSign = "Gemini";
+    } else if ((month == 6 && day >= 21) || (month == 7 && day <= 22)) {
+      zodiacSign = "Cancer";
+    } else if ((month == 7 && day >= 23) || (month == 8 && day <= 22)) {
+      zodiacSign = "Leo";
+    } else if ((month == 8 && day >= 23) || (month == 9 && day <= 22)) {
+      zodiacSign = "Virgo";
+    } else if ((month == 9 && day >= 23) || (month == 10 && day <= 22)) {
+      zodiacSign = "Libra";
+    } else if ((month == 10 && day >= 23) || (month == 11 && day <= 21)) {
+      zodiacSign = "Scorpio";
+    } else if ((month == 11 && day >= 22) || (month == 12 && day <= 21)) {
+      zodiacSign = "Sagittarius";
+    } else if ((month == 12 && day >= 22) || (month == 1 && day <= 19)) {
+      zodiacSign = "Capricorn";
+    } else if ((month == 1 && day >= 20) || (month == 2 && day <= 18)) {
+      zodiacSign = "Aquarius";
+    } else if ((month == 2 && day >= 19) || (month == 3 && day <= 20)) {
+      zodiacSign = "Pisces";
+    }
+  
+    const zodiacDiv = document.getElementById("zodiac");
+    zodiacDiv.innerHTML = `<p>Your zodiac sign is ${zodiacSign}.</p>`;
+  }
+  
